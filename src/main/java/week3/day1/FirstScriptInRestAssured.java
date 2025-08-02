7b426335f6d1bd6efea91b7fc7592140d13b360a
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-08-02 16:16:37 +0530

    week3.day1 code

---
+++ src/main/java/week3/day1/FirstScriptInRestAssured.java
@@ -0,0 +1,30 @@
+package week3.day1;
+
+import io.restassured.RestAssured;
+
+public class FirstScriptInRestAssured {
+	
+
+	public static void main(String[] args) {
+		
+	   /* given(), when(), then()
+	   *  given() -> set baseUri, basePath, auth, header, path and query param
+       *  when()  -> we need to call releavent http methods, get, post, put, patch and delete
+       *  then()  -> Assert response status code, body etc. extract value from response body
+       */ 
+		
+		RestAssured.given()
+		           .baseUri("https://dev271917.service-now.com")
+		           .basePath("/api/now/table")
+		           .auth().basic("admin", "ay*lPG$Va97D")
+		           .log().all()
+		           .when()
+		           .get("/incident")
+		           .then()
+		           .log().all()
+		           .assertThat()
+		           .statusCode(200);
+		
+	}
+
+}
